9ae95b37f82e588366eba2d876bf814a2c2b4a24
mackenza <a.mackenzie@lancaster.ac.uk>
2013-06-03 21:40:14 +0100

    added setup code for mysql; constructs db, tables, etc, on the fly

--- github_api_store_repos_in_db.py
+++ github_api_store_repos_in_db.py
@@ -1,5 +1,29 @@
-''' This is for making the 'repos' table for the data to go in
-CREATE TABLE  `git`.`repos` (
+
+""" Store and retrieve functions for github data.
+Configuration data, such as user and password  for
+databases needs to be in a local text file.
+
+Configuration
+----------------------------------
+The name of the database to use is held in the 
+global variabel DB_NAME
+ """
+
+import MySQLdb
+from pandas.io import sql
+import github_api_data as gad
+
+
+USER_FILE = open('github_api_user.txt')
+USER = USER_FILE.readline().rstrip('\n')
+PASSWORD = USER_FILE.readline().rstrip('\n')
+USER_FILE.close()
+DB_NAME = 'git_test'
+
+
+SQL_TABLES = {
+'repos':
+"""CREATE TABLE  IF NOT EXISTS `repos` (
 `id` INT( 11 ) NOT NULL ,
  `name` TEXT COLLATE utf8_bin NOT NULL ,
  `full_name` TEXT COLLATE utf8_bin NOT NULL ,
@@ -7,26 +31,53 @@ CREATE TABLE  `git`.`repos` (
  `description` TEXT COLLATE utf8_bin NOT NULL ,
  `fork` TINYINT( 1 ) NOT NULL ,
 PRIMARY KEY (  `id` )
-) ENGINE = INNODB DEFAULT CHARSET = utf8 COLLATE = utf8_bin
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COLLATE = utf8_bin;""",
 
-CREATE TABLE  `git`.`commits` (
+'commits': 
+"""CREATE TABLE  IF NOT EXISTS  `commits` (
 `date` TEXT NOT NULL ,
 `repository` TEXT COLLATE utf8_bin NOT NULL,
  `name` TEXT COLLATE utf8_bin NOT NULL ,
  `message` TEXT COLLATE utf8_bin NOT NULL ,
  `sha`  TEXT COLLATE utf8_bin NOT NULL );
-'''
+"""}
 
-import MySQLdb
-from pandas.io import sql
-import github_api_data as gad
 
-USER_FILE = open('github_api_user.txt')
-USER = USER_FILE.readline().rstrip('\n')
-PASSWORD = USER_FILE.readline().rstrip('\n')
-print USER, PASSWORD
-USER_FILE.close()
-DB = 'git_test'
+
+def db_setup(db_name = DB_NAME):
+
+    """ checks if database exists, creates it if not;
+    Also constructs tables because pandas.sql 
+    seems bad at doing this
+
+    Parameters
+    -----------------------------
+    db_name: name of database to check and setup 
+    """
+
+    try:
+        
+        #connect to DB_NAME if it exists
+        query = 'CREATE DATABASE IF NOT EXISTS %s;' % db_name
+        con =  MySQLdb.connect("localhost", 
+            USER, PASSWORD, db_name, charset='utf8')
+        cursor = con.cursor()
+        cursor.execute(query)
+
+        #check if tables exist
+        for table in SQL_TABLES:
+            
+            # if table doesn't exist, creat it
+            print 'creating table %s'  % table 
+            query = SQL_TABLES[table]
+            cursor.execute(query)
+        con.close()
+    except MySQLdb.MySQLError, sql_ex:
+        print sql_ex
+        return False
+    finally:
+        print 'database set up complete'
+    return True
 
 def save_repos(limit = 1000):
     
@@ -42,7 +93,7 @@ def save_repos(limit = 1000):
     repos_df = gad.get_repos(limit)
     try: 
         con = MySQLdb.connect("localhost", 
-            USER, PASSWORD, DB, charset='utf8')
+            USER, PASSWORD, DB_NAME, charset='utf8')
         sql.write_frame(
             repos_df[['id', 'name', 'private', 'full_name', 'description', 'fork']],  
             con=con,  name='repos',  
@@ -60,7 +111,7 @@ def read_repositories():
 
     try:
         con = MySQLdb.connect("localhost", 
-            USER, PASSWORD, DB, charset='utf8')
+            USER, PASSWORD, DB_NAME, charset='utf8')
         query = "select * from repos"
         repos_df = sql.read_frame(query, con)
     except MySQLdb.MySQLError, sql_ex:
@@ -84,7 +135,7 @@ def save_repo_commits(repository, limit  = 1000):
     df_temp = gad.get_repository_commits(repository, limit)
     try:
         con = MySQLdb.connect("localhost", 
-            USER, PASSWORD, DB, charset='utf8')
+            USER, PASSWORD, DB_NAME, charset='utf8')
         sql.write_frame(
                 df_temp,  
                 con=con,  name='commits',  
@@ -104,7 +155,7 @@ def read_repo_commits(repository):
 
     try:
         con = MySQLdb.connect("localhost", 
-            USER, PASSWORD, DB, charset='utf8')
+            USER, PASSWORD, DB_NAME, charset='utf8')
         query = "select * from commits where repository='%s'" % repository
         commits_df = sql.read_frame(query, con).unique()
     except MySQLdb.MySQLError, sql_ex:
