5356e243d68de7d9f4ad4e8aaa660d99e487b268
stuples <nope>
2014-04-14 10:44:11 +0100

    fix: 'create_db not found'

--- history-git/github_api/get_repo_names.py
+++ history-git/github_api/get_repo_names.py
@@ -1,51 +1,17 @@
 import datetime
 import github
 import requests
-import sys
 import textwrap
 import time
 import urllib
-
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is one of "yes" or "no".
-    """
-    valid = {"yes":True,   "y":True,  "ye":True,
-             "no":False,     "n":False}
-    
-    if default == None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-    
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        sys.stdout.write("\n")
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "\
-                             "(or 'y' or 'n').\n")
+import user_prompt
 
 def get_repo_names(self):
   if self.update_repo_names:
     question = textwrap.dedent("""
       Should I collect names of new repos? If this is
       the first time it will take at least 5 days.""")
-    ans = query_yes_no(question)
+    ans = user_prompt.query_yes_no(question)
     if ans == True:
       while True:
         try:
