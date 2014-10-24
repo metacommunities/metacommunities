465e3289535cad7fe2fb4cf750635b14bb86d976
rian39 <a.mackenzie@lancaster.ac.uk>
2014-10-24 21:47:46 +0100

    starting to reorganize github data

--- adrian/google_compute_startup.sh
+++ adrian/google_compute_startup.sh
@@ -14,6 +14,8 @@ ssh-add ~/.ssh/google_compute_engine
 
 ##ssh into instance
 gcutil ssh "instance-1"
+# using gcloud
+gcloud compute --project "237471208995" ssh --zone "europe-west1-b" "instance-1"
 
 #mount disk first
 sudo /usr/share/google/safe_format_and_mount -m "mkfs.ext4 -F" /dev/disk/by-id/scsi-0Google_PersistentDisk_disk-1 /git_data
@@ -25,6 +27,7 @@ sudo /etc/init.d/redis_6379 start
 # to get some github archive files
 wget http://data.githubarchive.org/2014-07-{01..31}-{0..23}.json.gz
 wget http://data.githubarchive.org/2014-08-{01..31}-{0..23}.json.gz
+wget http://data.githubarchive.org/2014-09-{01..31}-{0..23}.json.gz
 
 # to push/pull files
 gcutil push 'instance-1' repo_list.pyd /git_data
