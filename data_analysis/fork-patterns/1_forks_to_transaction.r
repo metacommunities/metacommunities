73e01f406dac0774671c94382c48fa28eca04ded
rian39 <a.mackenzie@lancaster.ac.uk>
2014-11-16 22:48:20 +0000

    trying apriori on fork pattern

--- data_analysis/fork-patterns/1_forks_to_transaction.r
+++ data_analysis/fork-patterns/1_forks_to_transaction.r
@@ -37,30 +37,48 @@ limit 500000"
 # to download the results of this query from Google BigQuery (if it has been saved in a storage bucket, and you have gsutil installed
 
  #gsutil cp gs://fork_actors_repos_500k/*.csv data/
-forks_500k = read.csv('data/fork_actors_repos_500k.csv')
+forks_500k = read.csv('data/fork_actors_repos_500k_2.csv')
 forks = forks_500k
 forks = read.csv('data/results-20141115-215517.csv')
-
+head(forks)
+# first cut them down manually to repos that are forked a lot
 fork_counts = as.data.frame(table(forks$actor_attributes_login))
 names(fork_counts) <- c('actor_attributes_login', 'count')
 top_forkers = fork_counts$actor_attributes_login[fork_counts$count>4]
 repo_count = as.data.frame(sort(table(forks$repository_name), decreasing=TRUE))
 colnames(repo_count) = c('count')
 top_repos = rownames(repo_count[repo_count$count>4,])
-top_basket = forks[forks$repository_name %in% top_repos & forks$actor_attributes_login %in% top_forkers,]
-summary(top_basket)
-top_basket$repo_count <- NULL
-top_basket = top_basket[order(top_basket$actor_attributes_login),]
-tbm = melt(top_basket, id = c('repository_name', 'actor_attributes_login'))
-dim(tbm)
-head(tbm) 
-tbm = unique(tbm[, 1:2])
-tbm1 = dlply(tbm, 'actor_attributes_login', function(x){x[,1]})
-head(tbm1)
-tail(tbm1)
-trans = as(sample(tbm1, 4000), 'transactions')
-image(trans)
-summary(trans)
-itemFrequencyPlot(trans, support=0.009, cex.names=0.7)
-rules = apriori(trans, parameter = list(support=0.01, confidence=0.6))
+top_repo_actors = forks[forks$repository_name %in% top_repos & forks$actor_attributes_login %in% top_forkers,]
+summary(top_repo_actors)
+top_repo_actors$repo_count <- NULL
+top_repo_actors = top_repo_actors[order(top_repo_actors$actor_attributes_login),]
+top_repo_actors_melted = melt(top_repo_actors, id = c('repository_name', 'actor_attributes_login'))
+dim(top_repo_actors_melted)
+head(top_repo_actors_melted) 
+top_repo_actors_melted = unique(top_repo_actors_melted[, 1:2])
+top_repo_actors_list = dlply(top_repo_actors_melted, 'actor_attributes_login', function(x){x[,1]})
+head(top_repo_actors_list)
+tail(top_repo_actors_list)
+length(top_repo_actors_list)
+trans_actor_fork = as(sample(top_repo_actors_list, 2000), 'transactions')
+image(trans_actor_fork)
+summary(trans_actor_fork)
+itemFrequencyPlot(trans_actor_fork, support=0.007, cex.names=0.7)
+rules = apriori(trans_actor_fork, parameter = list(support=0.003, confidence=0.3))
+summary(rules)
+inspect(rules[1:90])
+
+# without cutting anything in advance
+
+fork_melted = melt(forks, id = c('repository_name', 'actor_attributes_login'))
+fork_melted = unique(fork_melted[, 1:2])
+fork_list = dlply(fork_melted, 'actor_attributes_login', function(x){x[,1]})
+head(fork_list)
+tail(fork_list)
+length(fork_list)
+trans_actor_fork = as(sample(fork_list, 2000), 'transactions')
+image(trans_actor_fork)
+summary(trans_actor_fork)
+itemFrequencyPlot(trans_actor_fork, support=0.007, cex.names=0.7)
+rules = apriori(trans_actor_fork, parameter = list(support=0.003, confidence=0.3))
 summary(rules)
