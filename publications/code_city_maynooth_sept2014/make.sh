226d0e8c4516734be9bb57ee535afdeeb5096186
rian39 <a.mackenzie@lancaster.ac.uk>
2015-05-14 12:20:35 +0100

    draft sent to city book

--- publications/code_city_maynooth_sept2014/make.sh
+++ publications/code_city_maynooth_sept2014/make.sh
@@ -1,3 +1,3 @@
-pandoc --bibliography "/home/mackenza/Documents/ref_bibs/data_forms_thought.bib" --bibliography "/home/mackenza/Documents/ref_bibs/google_analytics.bib" --bibliography "/home/mackenza/Documents/ref_bibs/R.bib" --bibliography "/home/mackenza/Documents/ref_bibs/machine_learning.bib" -o "mackenze_code_city.pdf" "mackenze_code_city.md" --latex-engine=xelatex
-evince mackenze_code_city.pdf
+pandoc -f markdown+pipe_tables --template /home/mackenza/template.latex --bibliography /home/mackenza/Documents/ref_bibs/mackenzie.bib --bibliography "/home/mackenza/Documents/ref_bibs/data_forms_thought.bib" --bibliography "/home/mackenza/Documents/ref_bibs/google_analytics.bib" --bibliography "/home/mackenza/Documents/ref_bibs/R.bib" --bibliography "/home/mackenza/Documents/ref_bibs/machine_learning.bib" -o "mackenzie_code_city.pdf" "mackenzie_code_city.md" --latex-engine=xelatex
+evince mackenzie_code_city.pdf
 
