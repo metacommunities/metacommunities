e674f144a8ceb6ae503372cf976002d2a8523c6e
rian39 <a.mackenzie@lancaster.ac.uk>
2015-11-29 17:17:55 +0000

    starting to analysis gravatars

--- 2_gravatar_analysis.r
+++ 2_gravatar_analysis.r
@@ -1,19 +1,4 @@
-library(bigrquery)
-library(RCurl)
-library(png)
-library(jpeg)
+library(ggplot2)
 
-prj = 'metacommunities'
+df = read.csv('data_images/avatar_icon_sample_1000.csv')
 
-query  = "SELECT repository_url, payload_member_avatar_url FROM [githubarchive:github.timeline] where payload_member_avatar_url != '' LIMIT 1000"
-
-res =query_exec(query, project = prj)
-colnames(res) = c('repo_url', 'avatar_url')
-avatar_urls = unique(res$avatar_url)
-
-save_avatar <- function(url){
-    im = getURLContent(url)
-    #writePNG(im,file(paste(url, '.png', sep='')))
-}
-
-imgs = sapply(avatar_urls[1:5], save_avatar)
