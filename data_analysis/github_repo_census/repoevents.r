73e2f880086e3ae15dc84de15c726dc44fb98438
rian39 <a.mackenzie@lancaster.ac.uk>
2016-01-05 18:08:33 +0000

    fixing fork plots

--- data_analysis/github_repo_census/repoevents.r
+++ data_analysis/github_repo_census/repoevents.r
@@ -6,8 +6,11 @@ evf = read.csv('data/repo_event_counts_top1000_fork_repos.csv')
 evf$CumFreq = cumsum(evf$Freq)
 evf$Fork = TRUE
 ev$Fork=FALSE
+ev$Freq = ev$Freq - evf$Freq
 evdf = rbind(ev, evf)
 g = ggplot(evdf, aes(x=RepoEvents, y=Freq, group=Fork, color=Fork)) + geom_point() + scale_y_log10() + scale_x_log10()
 g + geom_hline(yintercept=c(10,100,1000,10000, 100000)) + geom_vline(xintercept=100)
 g + ggtitle('Repo events, contrasting forks and non-forks')
-ggsave('figures/repos_events_fork_not_fork.svg')
+ggsave('figures/repos_events_fork_not_fork_fixed.svg')
+g2 = ggplot(evdf, aes(x=RepoEvents, y=Freq, group=Fork, fill=Fork)) + geom_area() + scale_y_log10() + scale_x_log10()
+g2
