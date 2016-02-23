243843e83085f78871a966dfa9b94c1d4d7fefc9
rian39 <a.mackenzie@lancaster.ac.uk>
2016-02-23 22:51:23 +0000

    playing with script for archive of webpages

--- data_analysis/platform/github_website_overtime/2_tidying_urls.py
+++ data_analysis/platform/github_website_overtime/2_tidying_urls.py
@@ -1,20 +1,21 @@
 # coding: utf-8
 import pandas as pd
 import subprocess as sp
-get_ipython().magic(u'pinfo sp.Popen')
-urls = pd.read_csv('github.com_about.txt', header=None)
-get_ipython().magic(u'load 1_clean_archived_gh_websites.py')
 site = 'github.com/about'
 base = 'https://web.archive.org/web/'
-df['date_formatted'] = pd.to_datetime(df.date, format='%Y%m%d%H%M%S')
-full_url = base+df.date.astype(str) + '/'+df.url
-full_url.to_csv(site.replace('/', '_') + '.txt',index=False)
-df = pd.read_csv('full_github.com_about.csv')
-df.head()
 df = pd.read_csv('full_github.com_about.csv', header=None)
-df.head()
 df_c = df.iloc[:, [2,3,5,7]]
 df_c.columns = ['date', 'url', 'code', 'size']
+full_url = base+df_c.date.astype(str) + '/'+df_c.url
+df_c['full_url'] = full_url
+df_c.head()
 get_ipython().magic(u'matplotlib')
 df_c['size'].plot()
-df_c = df_c.ix[df_c['size']>500,['date', 'url']]
+# get pages with size greater than blank
+df_c = df_c.ix[df_c['size']>500,:]
+
+for i in df_c['full_url'].values:
+    r = sp.Popen(['wkhtmltoimage', i, i.replace('/', '_')+'.png'])
+
+
+
