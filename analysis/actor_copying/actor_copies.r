425c8c9bd57952c861ed6b005ab7dc5570f28906
rian39 <a.mackenzie@lancaster.ac.uk>
2016-02-28 21:32:17 +0000

    doing actor analysis

--- analysis/actor_copying/actor_copies.r
+++ analysis/actor_copying/actor_copies.r
@@ -7,30 +7,36 @@ group by actor_attributes_login
 order by events desc'
 project = 'metacommunities'
 #res = query_exec(query, project, ax_pages=Inf)
-res = read.csv('actors_events.csv')
+res = read.csv('actors_events.csv', as.is=TRUE)
+colnames(res) = c('actor_number', 'actor', 'events')
 head(res)
 tail(res)
-hist(res$events)
-g = ggplot(res, aes(x=events)) + geom_histogram(binwidth=2)
-ggplot(res, aes(x=events)) + geom_histogram(binwidth=2) + scale_y_log10()
-ggplot(res, aes(x=events)) + geom_histogram(binwidth=10) + scale_y_log10()
-actor_event_count = table(res$events)
+
+# this one is a cloud of points along the x asis
+g = ggplot(res[sample.int(n=nrow(res), 100000),], aes(x=actor, y=events)) + geom_point(size=0.3) + scale_y_log10()
+g = g + theme(axis.ticks = element_blank(), axis.text.x = element_blank()) 
+g
+ggsave(g, file = 'actor_events_unsorted.svg')
+
+# to order the results, tabulate
+actor_event_count = tabulate(res$events)
 actor_event_count = as.data.frame(actor_event_count)
-colnames(actor_event_count) = c('actors', 'events')
-actor_event_count$actors= as.numeric(actor_event_count$actors)
+colnames(actor_event_count) = c(  'actor_count')
+actor_event_count$event_count = as.numeric(rownames(actor_event_count))
+actor_event_count = actor_event_count[actor_event_count$actor_count>0,]
 head(actor_event_count)
-ggplot(actor_event_count, aes(x=actors, y=events)) + geom_point()
-ggplot(actor_event_count, aes(x=actors, y=events)) + geom_point() + scale_y_log10()
-ggplot(actor_event_count, aes(y=actors, x=events)) + geom_point() + scale_y_log10()
-ggplot(actor_event_count, aes(y=actors, x=events)) + geom_point() + scale_y_log10()
-ggplot(actor_event_count, aes(y=actors, x=events)) + geom_point(size=0.5) + scale_y_log10() + scale_x_log10()
-actor_event_count$actors[actor_event_count$events <2]
-sum(actor_event_count$actors[actor_event_count$events <2])
+tail(actor_event_count)
+g2 = ggplot(actor_event_count[], aes(y=actor_count, x=event_count)) + geom_point(size=0.5)
+g2 = g2 + theme(axis.ticks = element_blank(), axis.text.x = element_blank()) 
+g2 = g2 + scale_y_log10() + scale_x_log10() +  ggtitle('Events for actors')
+g2
+ggsave(g2, file='actor_events_counted.svg')
+
+actor_event_count$actor_count)actor_event_count$event_count)2]
+sum(actor_event_count$actors[actor_event_count$event_count)2])
 
 total_events = sum(res$events)
 
 first_50 = which(cumsum(res$events) > total_events)/2)[1]
 first_90 = which(cumsum(res$events) > 0.9*total_events)[1]
 first_90 
-g = ggplot(res[1:10000,], aes(x=actor_attributes_login, y=events)) + geom_point(size=0.5)
-g + theme(axis.ticks = element_blank(), axis.text.x = element_blank()) 
