0328979cb0b519d30e1accf063ca60ea570a5962
rian39 <a.mackenzie@lancaster.ac.uk>
2016-09-05 21:54:59 +0100

    tidying script to look at repo event counts and rank them

--- analysis/social_practices/repo_popularity_over_time.r
+++ analysis/social_practices/repo_popularity_over_time.r
@@ -7,33 +7,41 @@ library(reshape2)
 
 options(dplyr.width = Inf)
 
-query ="
-    SELECT
-        month(created_at) as month,
-        year(created_at) as year,
-        repo.name,
-        repo.url,
-        count(*) as events
-    FROM
-        [githubarchive:year.2012],
-        [githubarchive:year.2013],
-        [githubarchive:year.2014],
-        [githubarchive:year.2015] 
-    WHERE LENGTH(repo.name) >= 2 and type =='WatchEvent'
-    GROUP EACH BY year, month, repo.name, repo.url 
-    HAVING events > 300
-    ORDER BY year, month asc, events desc
-"
+event_rep_count <- function(type='ForkEvent', event_count=200){
+        query =paste("
+            SELECT
+                month(created_at) as month,
+                year(created_at) as year,
+                repo.name,
+                repo.url,
+                count(*) as events
+            FROM
+                [githubarchive:year.2011],
+                [githubarchive:year.2012],
+                [githubarchive:year.2013],
+                [githubarchive:year.2014],
+                [githubarchive:year.2015] 
+            WHERE LENGTH(repo.name) >= 2 AND type == '",
+            type, "'
+            GROUP EACH BY year, month, repo.name, repo.url 
+            HAVING events > ",
+            event_count,
+            " ORDER BY year, month ASC, events DESC",sep='')
 
-df = query_exec(query, project='metacommunities')
-head(df)
-dim(df)
-df = data.frame(df)
+        df = query_exec(query, project='metacommunities')
+        head(df)
+        df = data.frame(df)
+        return(df)
+}
 
-pos_df
-table(pos_df$year, pos_df$month)
+events_to_count = 200
+types = c('MemberEvent', 'PushEvent', 'PullRequestEvent', 'WatchEvent', 'ForkEvent', 'IssuesEvent')
+type = types[3]
+df = event_rep_count(type, events_to_count)
 pos_df = df %>% group_by(year, month) %>% mutate(pos =min_rank(desc(events))) %>% filter(pos <=10)
-write.csv(file = '../data/repos_most_watched_2012-2016.csv', pos_df)
+head(pos_df)
+file_name = paste('../data/repos_most_', type, '.csv', sep='')
+write.csv(file =file_name, pos_df)
 
 # reshape rankings into year x repo
 
